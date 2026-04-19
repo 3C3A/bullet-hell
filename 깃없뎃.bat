f35931a3b42b8ef2fe6f9b8c8448a4da77a3b627
3C3A <3C3A@users.noreply.github.com>
2026-04-19 19:15:22 +0900

    start THE bullet hell

--- 깃없뎃.bat
+++ 깃없뎃.bat
@@ -1,5 +1,6 @@
 git add *
-git branch -M main
 git commit -m "start THE bullet hell"
+git branch -M main
+git remote add origin https://github.com/3C3A/bullet-hell.git
 git push -u origin main
 pause
